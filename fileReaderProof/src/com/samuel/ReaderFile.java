699f681e22e17edf15f1b7fdb927aed4fef6b13d
haveANiceDay33 <munrosamuel@gmail.com>
2018-04-30 10:07:53 -0400

    Kinda getting somewhere. See potential problem with getting specific points

--- fileReaderProof/src/com/samuel/ReaderFile.java
+++ fileReaderProof/src/com/samuel/ReaderFile.java
@@ -9,6 +9,7 @@ public class ReaderFile {
 		  String angle;
 		  String type;
 		  String action;
+		  static int wayCount = 0;
 
 		  public ReaderFile(String distance, String angle, String type, String action){
 		       this.distance = distance;
@@ -18,16 +19,30 @@ public class ReaderFile {
 		  }
 
 		  public static void main(String args[]){
+			  float startX = 0;
+			  float startY = 0;
 		   try{
 		    FileInputStream fstream = new FileInputStream("Right Corner.txt");
 		          DataInputStream in = new DataInputStream(fstream);
 		          BufferedReader br = new BufferedReader(new InputStreamReader(in));
 		          String strLine;
-		          while ((strLine = br.readLine()) != null)   {
-		        	  String[] tokens = strLine.split(" ");
-		        	  ReaderFile record = new ReaderFile(tokens[0],tokens[1],tokens[2], tokens[3]);//process record , etc
+		          if(wayCount == 0) {
+		        	  strLine= br.readLine();
+		        	  String[] initItems = strLine.split(" ");
+		        	  ReaderFile start = new ReaderFile(initItems[0],initItems[1],initItems[2],initItems[3]);
+		        	  startX = Float.parseFloat(start.distance);
+		        	  startY = Float.parseFloat(start.angle);
+		        	  wayCount++;
 		          }
-		          in.close();
+		          if(wayCount > 0) {
+			          while ((strLine = br.readLine()) != null)   {
+			        	  String[] tokens = strLine.split(" ");
+			        	  ReaderFile waypoint = new ReaderFile(tokens[0],tokens[1],tokens[2], tokens[3]);//process record , etc
+			          }
+			          in.close();
+		          }
+		          System.out.println(startX);
+		          
 		   }catch (Exception e){
 		     System.err.println("Error: " + e.getMessage());
 		   }
